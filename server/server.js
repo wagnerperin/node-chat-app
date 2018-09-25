29dcfc6f2e9dca882a8800f871dd0a266ba06917
wagnerperin <wagnerperin@gmail.com>
2018-09-25 15:16:25 -0300

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -12,9 +12,21 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
     console.log('New user connected at> ' + socket.id);
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app'
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
         io.emit('newMessage', {...message, createdAt: new Date().getTime()});
+        // socket.broadcast.emit('newMessage', {...message, createdAt: new Date().getTime()})
     });
 
     socket.on('disconnect', () => {
